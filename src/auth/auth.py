f841e6ebf4f4f9311836c0dd954baab691018522
Mai Thien An <maian1891@gmail.com>
2024-03-16 23:35:37 +0700

    update environment

--- src/auth/auth.py
+++ src/auth/auth.py
@@ -1,3 +1,4 @@
+import os
 import json
 from flask import request, _request_ctx_stack
 from functools import wraps
@@ -5,9 +6,9 @@ from jose import jwt
 from urllib.request import urlopen
 
 
-AUTH0_DOMAIN = "dev-vnibk1kov3lnhttz.au.auth0.com"
-ALGORITHMS = ['RS256']
-API_AUDIENCE = "https://quickstarts/api"
+AUTH0_DOMAIN = os.environ['AUTH0_DOMAIN']
+ALGORITHMS = os.environ['ALGORITHMS']
+API_AUDIENCE = os.environ['API_AUDIENCE']
 
 # AuthError Exception
 '''
