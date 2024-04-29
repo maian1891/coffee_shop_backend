4412e37ecc418f4ffcca91ca65c8f348a998af04
Mai Thien An <maian1891@gmail.com>
2024-04-29 23:26:08 +0700

    update import

--- api.py
+++ api.py
@@ -6,8 +6,8 @@ from werkzeug.exceptions import HTTPException
 import traceback
 from flask_cors import CORS
 
-from .database.models import db_drop_and_create_all, setup_db, Drink
-from .auth.auth import AuthError, requires_auth
+from database.models import db_drop_and_create_all, setup_db, Drink
+from auth.auth import AuthError, requires_auth
 
 app = Flask(__name__)
 setup_db(app)
